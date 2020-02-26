e9dcffe31d62ac98b7d04fbce4821db5f5fe978e
wsjeon92 <44796831+wsjeon92@users.noreply.github.com>
2020-02-26 12:53:00 +0900

    Add files via upload

---
+++ optimize_BE.py
@@ -0,0 +1,163 @@
+# coding=utf-8
+# Copyright 2019 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# modified by W. Jeon 2020
+
+# Lint as: python2, python3
+"""Optimizes a binding affinity of a molecule against the target with MolDQN.
+MORLD tries to find the molecule with the highest docking score
+starting from a given molecule.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import functools
+import json
+import os
+
+# you need to add path of mol_dqn and gym-molecule. Example,
+# import sys
+# sys.path.append("~/google-research")
+# sys.path.append("~/rl_graph_generation/gym-molecule")
+
+
+from absl import app
+from absl import flags
+
+from rdkit import Chem
+
+from rdkit.Chem import QED
+from tensorflow import gfile
+from mol_dqn.chemgraph.dqn import deep_q_networks_parent
+from mol_dqn.chemgraph.dqn import molecules as molecules_mdp
+from mol_dqn.chemgraph.dqn import run_dqn_parent
+from mol_dqn.chemgraph.dqn.tensorflow_core import core
+
+FLAGS = flags.FLAGS
+
+from gym_molecule.envs.sascorer import calculateScore
+import pandas as pd
+
+# Molecule class for the reward of binding affinity
+class BARewardMolecule(molecules_mdp.Molecule):
+  """The molecule whose reward is the Bingding affinity."""
+
+  def __init__(self, discount_factor, **kwargs):
+    """Initializes the class.
+
+    Args:
+      discount_factor: Float. The discount factor. We only
+        care about the molecule at the end of modification.
+        In order to prevent a myopic decision, we discount
+        the reward at each step by a factor of
+        discount_factor ** num_steps_left,
+        this encourages exploration with emphasis on long term rewards.
+      **kwargs: The keyword arguments passed to the base class.
+    """
+    super(BARewardMolecule, self).__init__(**kwargs)
+    self.discount_factor = discount_factor
+
+  def _reward(self):
+    """Reward of a state.
+
+    Returns:
+      intermediate reward: SA score, QED score
+      final reward: Docking score (a negative value of the binding energy)
+    """
+    molecule = Chem.MolFromSmiles(self._state)
+    if molecule is None:
+      return 0.0
+
+    # calculate SA and QED score
+    sa = calculateScore(molecule)
+    sa_norm = round((10-sa)/9,2) # normalize the SA score
+    qed = round(QED.qed(molecule),2)
+    print("SA score and QED: {}, {} : {}".format(sa_norm, qed, self._state))
+
+    if self._counter < self.max_steps: # intermediate state
+      return round((sa_norm+qed)*self.discount_factor ** (self.max_steps - self.num_steps_taken),2)
+
+    if self._counter >= self.max_steps: # terminal state
+      # create SMILES file
+      with open('ligand.smi','w') as f:
+        f.write(self._state)
+
+      # convert SMILES > PDBQT
+      # --gen3d: the option for generating 3D coordinate
+      #  -h: protonation
+      cvt_cmd = "obabel ligand.smi -O ligand.pdbqt --gen3D -h > cvt_log.txt"
+      os.system(cvt_cmd)
+
+      # docking
+      docking_cmd ="qvina02 --config config.txt --num_modes=1 > log_docking.txt"
+      os.system(docking_cmd)
+
+      # parsing docking score from log file
+      try:
+        data = pd.read_csv('log_docking.txt', sep= "\t",header=None)
+      except:
+        return 0.0
+      docking_score = round(float(data.values[-2][0].split()[1]),2)
+      print("binding energy value: "+str(round(docking_score,2))+'\t'+self._state)
+
+    # record a optimized result with the SMILES, docking score, SA score,
+    # and QED score.
+    with open('./optimized_result_total.txt', 'a') as f2:
+      f2.write(self._state+'\t'+str(docking_score)+'\t'+str(sa_norm)+'\t'+str(qed)+'\n')
+
+    # we use the negative of the docking score because the lower docking score
+    # the better.
+    return round(-docking_score, 2)
+
+
+def main(argv):
+  del argv  # unused.
+  if FLAGS.hparams is not None:
+    with gfile.Open(FLAGS.hparams, 'r') as f:
+      hparams = deep_q_networks_parent.get_hparams(**json.load(f))
+  else:
+    hparams = deep_q_networks_parent.get_hparams()
+  environment = BARewardMolecule(
+      discount_factor=hparams.discount_factor,
+      atom_types=set(hparams.atom_types),
+      init_mol= FLAGS.start_molecule,
+      allow_removal=hparams.allow_removal,
+      allow_no_modification=hparams.allow_no_modification,
+      allow_bonds_between_rings=hparams.allow_bonds_between_rings,
+      allowed_ring_sizes=set(hparams.allowed_ring_sizes),
+      max_steps=hparams.max_steps_per_episode)
+
+  dqn = deep_q_networks_parent.DeepQNetwork(
+      input_shape=(hparams.batch_size, hparams.fingerprint_length + 1),
+      q_fn=functools.partial(
+          deep_q_networks_parent.multi_layer_model, hparams=hparams),
+      optimizer=hparams.optimizer,
+      grad_clipping=hparams.grad_clipping,
+      num_bootstrap_heads=hparams.num_bootstrap_heads,
+      gamma=hparams.gamma,
+      epsilon=1.0)
+
+  run_dqn_parent.run_training(
+      hparams=hparams,
+      environment=environment,
+      dqn=dqn)
+
+  core.write_hparams(hparams, os.path.join(FLAGS.model_dir, 'config_sa.json'))
+
+
+if __name__ == '__main__':
+  app.run(main)
