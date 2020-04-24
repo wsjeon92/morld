66887587f332358602a2a98d6d8110d313b7a7c6
wsjeon92 <44796831+wsjeon92@users.noreply.github.com>
2020-04-24 17:40:47 +0900

    Update optimize_BE.py

--- optimize_BE.py
+++ optimize_BE.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # Copyright 2019 The Google Research Authors.
-# Modified Copyright 2020 by Woosung Jeon.
+# Modified Copyright 2020 by Woosung Jeon and Dongsup Kim.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
